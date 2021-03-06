fc0ea3658bd1d04fd4a71eed2cd4bdb644b19e47
kedley <kedley@gmail.com>
2021-03-06 17:29:56 -0300

    footer

---
+++ src/app/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, FooterText } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+      <Container>
+          <FooterText>Desenvolvido por Felipe Mignone e Kedley Tofoli</FooterText>
+      </Container>
+  );
+}
+
+export default Footer;
