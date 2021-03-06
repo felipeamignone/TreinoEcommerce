f820657802e5962ff8155321bd183c139306a602
felipeamignone <dev.felipemignone@gmail.com>
2021-03-06 17:12:54 -0300

    Home Icon

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { Container,  HomeText } from "./styles";
+import Home from "../../assets/home.png";
+
+import { Container, HomeButton, HomeIcon, HomeText } from "./styles";
 
 const Header: React.FC = () => {
   return (
     <Container>
-        <HomeText>e-commerce de alface</HomeText>
+      <HomeButton>
+        <HomeIcon src={Home} alt="Icone menu" />
+      </HomeButton>
+      <HomeText>e-commerce de alface</HomeText>
     </Container>
   );
 };
--- src/app/Header/styles.ts
+++ src/app/Header/styles.ts
@@ -2,9 +2,23 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 54px;
   background-color: blueviolet;
 `;
 
-export const HomeText = styled.p``;
+export const HomeText = styled.p`
+  color: #fcfcfc;
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const HomeButton = styled.button`
+  border: 0;
+  background: transparent;
+`;
+
+export const HomeIcon = styled.img``;
