f26ada092c4000e17d31c2527c074c28625aebfe
felipeamignone <dev.felipemignone@gmail.com>
2021-03-06 17:26:01 -0300

    add routes

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      {/* <Route path="/" exact component={Landing} /> */}
+    </Switch>
+  );
+};
+
+export default Routes;
